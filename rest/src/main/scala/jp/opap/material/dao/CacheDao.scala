de931d937be2d111c9a1e52fb47253ba1ca45448
bromne <bromne@gmail.com>
2018-05-05 10:15:32 +0900

    フロントエンド用リポジトリとして、ファイルを移動しました。

--- rest/src/main/scala/jp/opap/material/dao/CacheDao.scala
+++
@@ -1,7 +0,0 @@
-package jp.opap.material.dao
-
-trait CacheDao {
-  def insert(key: String, data: Array[Byte]): Unit
-  def findByKey(key: String): Option[Array[Byte]]
-  def loadIfAbsent(key: String, supplier: String => Array[Byte]): Array[Byte]
-}
--- rest/src/main/scala/jp/opap/material/model/Thumbnail.scala
+++
@@ -1,5 +0,0 @@
-package jp.opap.material.model
-
-import scala.beans.BeanProperty
-
-case class Thumbnail(@BeanProperty id: String, @BeanProperty width: Int, @BeanProperty height: Int)
